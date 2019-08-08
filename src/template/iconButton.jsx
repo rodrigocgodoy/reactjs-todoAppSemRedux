c8c6cd60cd7402848b3b1d4fed38eb1bbfe3b078
Rodrigo Godoy <rodrigocgodoy@hotmail.com>
2019-08-08 13:40:24 -0300

    primeiro commit

---
+++ src/template/iconButton.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import If from './if'
+
+export default props => (
+    <If test={!props.hide}>
+        <button 
+            className={'btn btn-'+ props.style} 
+            onClick={props.onClick}
+            title={props.title}
+        >
+            <i className={'fa fa-'+ props.icon}></i>
+        </button>
+    </If>
+) 
